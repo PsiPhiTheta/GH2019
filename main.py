03d2d546b2d66aaea60d28b9a0cf93bd7c04517d
Nicolo <nic.frisiani@gmail.com>
2019-02-28 19:08:58 +0000

    added verticals

--- main.py
+++ main.py
@@ -11,3 +11,18 @@ if __name__ == "__main__":
     sorted_photos = get_sorted_photos(input["photos"])
     print(sorted_photos[0].tags[0])
 
+
+def longass_tags(vertical_photos):
+    vertical_slides = [[]]
+    total_photos = len(vertical_photos)
+    common_tags = 0
+    
+    while(len(vertical_slides) < int(total_photos - 1)/2):
+        for i in range(1, len(vertical_photos)):
+            if(len(set(vertical_photos[i-1].tags) & set(vertical_photos[i].tags)) <= common_tags):
+                vertical_slides.append([vertical_photos[i-1], vertical_photos[i]])
+                vertical_photos.pop(i-1)
+                vertical_photos.pop(i)
+        common_tags += 1
+    
+    return vertical_slides
