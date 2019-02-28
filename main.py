452965adfad7170d4eeffa973259dc46a4c1f8e2
Raul Lozano Martin <raullm7@gmail.com>
2019-02-28 19:01:13 +0000

    Sort photos by length of tags

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+def get_sorted_photos(photos):
+    photos.sort(key=lambda x: len(x.tags), reverse=True)
+    return photos
+
+
 if __name__ == "__main__":
     import sys
     import process_input as ps
     input = ps.process_input(sys.argv[1])
-    for tag in input["photos"][1].tags:
-        print(tag)
+
+    sorted_photos = get_sorted_photos(input["photos"])
+    print(sorted_photos[0].tags[0])
+
